15d33038a7351af71baf57b1f4ebea38f44948f8
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-22 20:58:06 +0000

    Item11887: bump version
    git-svn-id: http://svn.foswiki.org/trunk/TagMePlugin@14883 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TagMePlugin.pm
+++ lib/Foswiki/Plugins/TagMePlugin.pm
@@ -16,7 +16,7 @@ use vars qw(
 );
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '2.0.1';
+our $RELEASE           = '2.0.2';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION =
   'Tag wiki content collectively to find content by keywords';
